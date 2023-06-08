fae76173897ba4885b10a8efce990dad511b3538
Shreyas Pathak <97246657+shreyaspathak11@users.noreply.github.com>
2023-06-08 22:58:50 +0530

    user_API_runnimg

--- server/index.js
+++ server/index.js
@@ -11,10 +11,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Import routes
-const post = require('./routes/post');
+const post = require('./routes/Post');
+const user = require('./routes/User');
 
 // Using Routes
 app.use('/api/v1', post);
+app.use('/api/v1', user);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server listening on port ${process.env.PORT}`);
