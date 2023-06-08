50239a20025c2b1ff1c769aa4c6839d94fd8eff6
Shreyas Pathak <97246657+shreyaspathak11@users.noreply.github.com>
2023-06-08 23:58:31 +0530

    postman working

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');   // Import mongoose
+const bcrypt = require('bcrypt');     // Import bcryptjs
+const jwt = require('jsonwebtoken');   // Import jsonwebtoken
+
 
 const userSchema = new mongoose.Schema({    // Create a new schema
     name: {
@@ -49,5 +52,22 @@ const userSchema = new mongoose.Schema({    // Create a new schema
     ],
 
 });
+
+userSchema.pre('save', async function (next) {    // Encrypt password before saving user
+    if (this.isModified('password')) {              // Check if password is modified
+     this.password = await bcrypt.hash(this.password, 10);              // Encrypt password
+    }
+
+    next();             // if password is modified, then move to next middleware
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {    // Compare entered password with hashed password
+    return await bcrypt.compare(enteredPassword, this.password);         // Return true or false
+};
+
+userSchema.methods.generateToken = async function () {    // method to Generate token
+    const token = await jwt.sign({ id: this._id }, process.env.JWT_SECRET, );    // Generate token
+    return token;       // Return token
+};
 
 module.exports = mongoose.model('User', userSchema); // Export the model`
