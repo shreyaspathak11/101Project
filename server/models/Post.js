444129ec1149d51d6de81be88e2eb8b4ab38d28d
Shreyas Pathak <97246657+shreyaspathak11@users.noreply.github.com>
2023-06-12 23:52:12 +0530

    comments added

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,36 +1,38 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose");           //import mongoose for creating schema
 
-const postSchema = new mongoose.Schema({
-  caption: String,
+const postSchema = new mongoose.Schema({          //create schema of POST
+  // Schema contains the following fields:
+  caption: String,                              //caption of post             
 
-  image: {
+  image: {                                    //image of post               
     public_id: String,
     url: String,
   },
 
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+  owner: {                                 //owner of post  
+    type: mongoose.Schema.Types.ObjectId,       //type of owner here object id mongoose.schema.type.objectid means id of owner
+    ref: "User",                                //ref means reference to user
   },
 
-  createdAt: {
-    type: Date,
-    default: Date.now,
+  createdAt: {                            //created at of post                  
+    type: Date,                        //type is date           
+    default: Date.now,              //default value of created at is date.now
   },
-  likes: [
+
+  likes: [                      //likes of post
     {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
+      type: mongoose.Schema.Types.ObjectId,       //type of likes here object id mongoose.schema.type.objectid means id of likes
+      ref: "User",                              //ref means reference to user               
+    },  
   ],
 
-  comments: [
+  comments: [                  //comments of post contains 2 parts  1. user 2. comment
     {
-      user: {
+      user: {                               //user of comment                    
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
       },
-      comment: {
+      comment: {                          //comment of comment            
         type: String,
         required: true,
       },
@@ -38,4 +40,4 @@ const postSchema = new mongoose.Schema({
   ],
 });
 
-module.exports = mongoose.model("Post", postSchema);
+module.exports = mongoose.model("Post", postSchema);  //export post schema  
