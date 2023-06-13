2f5361fa5940ebc6c6396e5a8ee69f3778dae4a9
Shreyas Pathak <97246657+shreyaspathak11@users.noreply.github.com>
2023-06-14 02:41:49 +0530

    frontend first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header/Header";
-import Login from "./Components/Login/Login";
 
-function App() {
-  const { isAuthenticated } = useSelector((state) => state.user);
-
+function    App() {
   return (
 
     <Router>
-      {isAuthenticated && <Header />}
 
-      <Routes>
-        <Route path="/" element={isAuthenticated ? <Home /> : <Login />} />
-        <Route
-          path="/account"
-          element={isAuthenticated ? <Account /> : <Login />}
-        />
-        </Routes>
+
+      <Header />
     </Router>
   );
 }
